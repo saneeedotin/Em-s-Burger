0eedf34683076a9df483b8cd3f366a2458e77440
hjsolanki5454-png <hjsolanki5454@gmail.com>
2026-08-22 13:01:02 +0530

    Fix image URL spacing bug in FlowingMenu to display burger images

--- src/components/FlowingMenu.jsx
+++ src/components/FlowingMenu.jsx
@@ -155,7 +155,7 @@ function MenuItem({ link, text, image, speed, textColor, marqueeBgColor, marquee
             {[...Array(repetitions)].map((_, idx) => (
               <div className="marquee__part" key={idx} style={{ color: marqueeTextColor }}>
                 <span>{text}</span>
-                <div className="marquee__img" style={{ backgroundImage: `url(${image})` }} />
+                <div className="marquee__img" style={{ backgroundImage: `url("${image}")` }} />
               </div>
             ))}
           </div>
